b8a9ceb14100f2e16c26d01d0b860b475e2f6271
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-04 10:26:25 +0200

    Gjort klart "skapa en kommentar"-funktionen.

--- funktioner/skapaKommentar.php
+++ funktioner/skapaKommentar.php
@@ -3,9 +3,9 @@
     $UID = mysqli_real_escape_string($conn, $_REQUEST['UID']); //Användar-ID
     $IID = mysqli_real_escape_string($conn, $_REQUEST['IID']); //Blogginlägg-ID
     $text = mysqli_real_escape_string($conn, $_REQUEST['text']); //Kommentar text
-    $heirarchyID = mysqli_real_escape_string($conn, $_REQUEST['heirarchyID']);
+    $hierarchyID = mysqli_real_escape_string($conn, $_REQUEST['hierarchyID']);
 
-    $sql = "INSERT INTO kommentar (UID, IID, text, heirarchyID) VALUES ('$UID', '$IID', '$text', '$heirarchyID')";
+    $sql = "INSERT INTO kommentar (UID, IID, text, hierarchyID) VALUES ('$UID', '$IID', '{$text}', '$hierarchyID')";
     if(mysqli_query($conn, $sql)){
         echo "INFO: Kommentar skapad.";
     } else{
