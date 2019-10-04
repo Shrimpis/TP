9d9b4d546b04ac7c2965530c057e8351256e319c
Joel Magnusson <joel.magnusso@elev.ga.ntig.se>
2019-10-04 14:46:35 +0200

    Improved delete for blogg

--- funktioner/tabortblogg.php
+++ funktioner/tabortblogg.php
@@ -2,11 +2,25 @@
     include('dbh.inc.php');
     $BID = mysqli_real_escape_string($conn, $_REQUEST['BID']);
     $sql = "DELETE FROM blogg WHERE BID='{$BID}'";
-    // $sql2 = "DELETE FROM blogginlagg WHERE BID='{$BID}' &&mysqli_query($conn, $sql2)";
+    $sql2 = "DELETE FROM blogginlagg WHERE BID='{$BID}'";
 
-    if(mysqli_query($conn, $sql)){
+    $IIDarray = ($conn->query("SELECT IID from blogginlagg where BID ='{$BID}'"));
+    
+    while($row = $IIDarray->fetch_assoc()){
+        
+        
+        $IID= $row['IID'];
+        
+        $sql3="DELETE FROM textruta WHERE IID=$IID";
+        $sql4="DELETE FROM rutor WHERE IID=$IID";
+        
+        $conn->query($sql3);
+        $conn->query($sql4);
+        
+    }
+    if(mysqli_query($conn, $sql)&&mysqli_query($conn, $sql2)){
         echo "INFO: Blogg borttagen";
     } else {
-        echo "ERROR: Could not execute $sql. " . mysqli_error($conn);
+        echo "ERROR: Could not execute $sql,$sql2. " . mysqli_error($conn);
     }
 ?>
--- funktioner/tabortinlagg.php
+++ funktioner/tabortinlagg.php
@@ -2,12 +2,12 @@
     include('dbh.inc.php');
     $IID = mysqli_real_escape_string($conn, $_REQUEST['IID']);
     $sql = "DELETE FROM blogginlagg WHERE IID='{$IID}'";
-    // $sql2 = "DELETE FROM rutor WHERE IID='{$IID}'";
-    // $sql3 = "DELETE FROM textruta WHERE IID='{$IID}' &&mysqli_query($conn, $sql2)&&mysqli_query($conn, $sql3)";
+    $sql2 = "DELETE FROM rutor WHERE IID='{$IID}'";
+    $sql3 = "DELETE FROM textruta WHERE IID='{$IID}'";
 
-    if(mysqli_query($conn, $sql)){
+    if(mysqli_query($conn, $sql)&&mysqli_query($conn, $sql2)&&mysqli_query($conn, $sql3)){
         echo "INFO: Blogginlagg borttaget";
     } else {
-        echo "ERROR: Could not execute $sql. " . mysqli_error($conn);
+        echo "ERROR: Could not execute $sql,$sql2,$sql3. " . mysqli_error($conn);
     }
 ?>
