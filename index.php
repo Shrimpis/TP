f07de4a313e43173a3bd7bf3948cf23651036805
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-09 09:19:15 +0200

    Fixade problem med visablogg.js

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-        <script src="js/visablogg.js"></script>
+        
         <script src="js/skapainlagg.js"></script>
     </head>
 
@@ -456,4 +456,5 @@
         </div>
         </div>
     </body>
+    <script src="js/visablogg.js"></script>
 </html>
