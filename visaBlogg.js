d3d0fcab8e65ee0c07506d60d6f87ca7d3a0cd11
Alvar Ljungfjär <ljungfjaralvar@gmail.com>
2019-10-09 12:15:09 +0200

    sparar innan jag gör reklusiv function

--- visaBlogg.js
+++ visaBlogg.js
@@ -94,12 +94,19 @@ function createInlagg(id) {
 
 
 //Inte dynamisk
-function createKommentar() {
+function createKommentar(kom) {
     let body = document.getElementById("kommentarContainer");
     let kommentar = document.createElement("div");
 
     for(let i = 0; i < jsonData.bloggInlagg.length; i++)
         for (let j = 0; j < jsonData.bloggInlagg[i].kommentarer.length; j++) {
+            
+            if(kom.length > 0) {
+                for(let komLangd = 0; komLangd < kom.length; komLangd++) {
+                    kommentarArray.push(kom[komLangd]);
+            }
+                createKommentar(kom[j]);
+            }
 
             let element = document.createElement("div");
             element.id = "kommentar" + j;
@@ -110,46 +117,13 @@ function createKommentar() {
             element.appendChild(element2);
 
             try{
-              console.log(jsonData.bloggInlagg[i].kommentarer[j].kommentarer[1].kommentarer[2].text);  
+              console.log(jsonData.bloggInlagg[i].kommentarer[j].kommentarer[1].text);  
 
               lista = jsonData.bloggInlagg[i].kommentarer;
-              alert(lista[0]);
+              alert(lista.length);
             }
             catch(error){}
-            
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            /*if (jsonData.bloggInlagg[i].kommentarer[j].hierarchyID == 0) {
-                element2 = document.createElement("p");
-                element2.innerHTML = jsonData.bloggInlagg[i].kommentarer[j].namn + ": " + jsonData.bloggInlagg[i].kommentarer[j].text;
-                element.appendChild(element2);
-            } else {
-                element2 = document.createElement("div");
-                let element3 = document.createElement("p");
-                element3.innerHTML = jsonData.bloggInlagg[i].kommentarer[j].namn + ": " + jsonData.bloggInlagg[i].kommentarer[j].text;
-
-                element2.appendChild(element3);
-                element.appendChild(element2);
-            }*/
-            
             kommentar.appendChild(element);
   
     }
