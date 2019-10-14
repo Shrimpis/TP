7f55aa205a88cadaeaee958e8dc622e00137f0ff
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-14 12:44:28 +0200

    Lagt till inlogningsfunktionen.

--- Admin/login.php
+++ Admin/login.php
@@ -1,3 +1,28 @@
+<?php 
+   include ('includes/dbh.inc.php');
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      
+      $username = mysqli_real_escape_string($conn,$_POST['anamn']);
+      $pwd = mysqli_real_escape_string($conn,$_POST['losenord']);
+      
+      $sql = "SELECT anamn FROM anvandare WHERE anamn = '$username' AND losenord = '$pwd' AND role_id='1'";
+      $result = mysqli_query($conn,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      
+      $count = mysqli_num_rows($result);
+      
+		
+      if($count == 1) {
+         $_SESSION['login_user'] = $username;
+         header("location: index.php");
+      }else {
+         $error = "Failed to login.";
+         header("location: ./login.php?userLogin=Error?reason=NotAdmin");
+      }
+   }
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -19,9 +44,9 @@
   <body class="text-center">
     <form class="form-signin" action = "" method = "post">
       <h1 class="h3 mb-3 font-weight-normal">Logga in till Adminpanelen</h1>
-      <input type="text" id="username" name="username" class="form-control" placeholder="Användarnamn" required autofocus>
+      <input type="text" id="anamn" name="anamn" class="form-control" placeholder="Användarnamn" required autofocus>
       <br>
-      <input type="password" id="pwd" name="pwd" class="form-control" placeholder="Lösenord" required autofocus>
+      <input type="password" id="losenord" name="losenord" class="form-control" placeholder="Lösenord" required autofocus>
       <br>
       <button class="btn btn-lg btn-primary btn-block" type="submit" value="Submit">Logga in</button>
       <p class="mt-5 mb-3 text-muted"><?php include ('includes/settings.php'); echo $site_footer?></p>
