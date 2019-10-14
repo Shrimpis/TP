384f41d4cbf62ca34ced4bfd7190d3a32dd9ca90
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-14 12:24:24 +0200

    Skapat logout.php

--- Admin/logout.php
+++ Admin/logout.php
@@ -1,15 +1,26 @@
-<!DOCTYPE html>
-<html class="no-js">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
+    <title><?php include 'includes/settings.php'; echo $site_title ?> - Loggar ut...</title>
 
-    </body>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="http://<?php echo $domain_name ?>/assets/css/signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+    <form class="form-signin" action = "" method = "post">
+      <h1 class="h3 mb-3 font-weight-normal">Loggar ut...</h1>
+      <p class="mb-3 text-muted">Du loggas nu ut från ditt konot</p>
+    </form>
+  </body>
 </html>
