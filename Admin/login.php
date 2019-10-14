e37abf117d34d83f04570122a5b379863db6ec4d
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-14 12:25:29 +0200

    Skapat login.php

--- Admin/login.php
+++ Admin/login.php
@@ -1,15 +1,29 @@
-<!DOCTYPE html>
-<html class="no-js">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
+    <title><?php include 'includes/settings.php'; echo $site_title ?> - Logga in</title>
 
-    </body>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link href="http://<?php echo $domain_name ?>/assets/css/signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+    <form class="form-signin" action = "" method = "post">
+      <h1 class="h3 mb-3 font-weight-normal">Logga in till ditt konto</h1>
+      <label for="inputEmail" class="sr-only" >Personnummer</label>
+      <input type="password" id="inputPersnr" name="inputPersnr" class="form-control" placeholder="20001212XXXX" maxlength="12" required autofocus>
+      <br>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" value="Submit">Logga in</button>
+      <p class="mt-5 mb-3 text-muted"><?php include ('includes/settings.php'); echo $login_footer?></p>
+    </form>
+  </body>
 </html>
