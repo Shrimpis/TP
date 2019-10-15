4e037e7061e5775cc77af24b207f15478389aeee
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-15 12:07:15 +0200

    Ändrade navbar.

--- Admin/index.php
+++ Admin/index.php
@@ -63,13 +63,14 @@ if(!isset($_SESSION['login_user'])){
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
           <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
+          <span>Kund</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
+            <h6 class="collapse-header">Kund inställningar</h6>
+            <a class="collapse-item" href="#tjanst">Tjänster</a>
+            <a class="collapse-item" href="#tabortsuperadmin">Ta bort superadmin</a>
+            <a class="collapse-item" href="#tabortsuperadmin">Ta bort blogg</a>
           </div>
         </div>
       </li>
