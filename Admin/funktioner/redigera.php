fa6be7ad56e253686207eb32c9a5a0c63927c2bd
adolf pizzamannen <joel.magnusso@elev.ga.ntig.se>
2019-10-15 14:09:18 +0200

    fixade redigerafunktionen

--- Admin/funktioner/redigera.php
+++ Admin/funktioner/redigera.php
@@ -9,28 +9,60 @@ include("dbh.inc.php");
             case 'redigeraKonto':
                 redigeraKonto();
                 break;
+            case 'redigeraRoll':
+                redigeraRoll();
+                break;
             default:
                 echo "ERROR: Något fel med URL-parametrarna för din begäran. Kontrollera dokumentationen.";
+                break;
         }
    
 $conn->close();
 
 function redigeraKonto(){
     include("dbh.inc.php");
-    if(isset($_POST['anamn']) && isset($_POST['losenord']) && isset($_POST['id'])){
-        $UID = $_POST['id'];
-	$anamn = $_POST['anamn'];
+    if(isset($_POST['anamn']) && isset($_POST['losenord']) && isset($_POST['UID'])){
+        $UID = $_POST['UID'];
+	    $anamn = $_POST['anamn'];
         $losenord = $_POST['losenord'];
     }
-    $uppdateraAnamn = "UPDATE anvandare SET anamn = '{$anamn}' WHERE UID = $UID ";
-    $uppdateraLosenord = "UPDATE anvandare SET losenord = '{$losenord}'  WHERE UID = $UID ";
+    if(isset($_POST['fnamn']) && isset($_POST['enamn']) && isset($_POST['email'])){
+        $fnamn = $_POST['fnamn'];
+        $enamn = $_POST['enamn'];
+        $email = $_POST['email'];
+    }
+    $uppdateraAnamn = "UPDATE anvandare SET anamn = '{$anamn}' WHERE id = $UID ";
+    $uppdateraLosenord = "UPDATE anvandare SET losenord = '{$losenord}'  WHERE id = $UID ";
+    $uppdaterafnamn = "UPDATE anvandare SET fnamn = '{$fnamn}'  WHERE id = $UID ";
+    $uppdateraenamn = "UPDATE anvandare SET enamn = '{$enamn}'  WHERE id = $UID ";
+    $uppdateraemail = "UPDATE anvandare SET email = '{$email}'  WHERE id = $UID ";
     
     
-    if(mysqli_query($conn, $uppdateraAnamn)&&mysqli_query($conn, $uppdateraLosenord)){
+    if(mysqli_query($conn, $uppdateraAnamn)&&mysqli_query($conn, $uppdateraLosenord)&&mysqli_query($conn, $uppdateraenamn)&&mysqli_query($conn, $uppdaterafnamn)&&mysqli_query($conn, $uppdateraemail)){
         echo "INFO: kontot har redigerats.";
-        header('Refresh: 2; URL = ../redigerakontoform.php');
+        header('Refresh: 2; URL = ../kontoformsadmin.php');
     } else {
         echo "ERROR: Could not execute $uppdateraAnamn. " . mysqli_error($conn);
+    }
+    $conn->close();
+}
+function redigeraRoll(){
+    include("dbh.inc.php");
+    if(isset($_POST['UID']) && isset($_POST['rollid'])){
+        $UID = $_POST['UID'];
+	    $roll = $_POST['rollid'];
+        
+    }
+    $uppdateraRoll= "UPDATE anvandarroll SET rollid = '{$roll}' where anvandarid = $UID";
+    
+    
+    
+    
+    if(mysqli_query($conn, $uppdateraRoll)){
+        echo "INFO: kontot har redigerats.";
+        header('Refresh: 2; URL = ../kontoformsadmin.php');
+    } else {
+        echo "ERROR: Could not execute $uppdateraRoll. " . mysqli_error($conn);
     }
     $conn->close();
 }
--- Admin/kontoformsadmin.php
+++ Admin/kontoformsadmin.php
@@ -1,12 +1,19 @@
 <!-- Redigera konto -->
 
-<h4>Redigera kontoredigerare</h4>
+<h4>Redigera kontoredigerare (User)</h4>
 
 
 <form action="funktioner/redigera.php" method="POST">
 <input type='hidden' name='funktion' value='redigeraKonto'/>
-    <input type="text" name="anamn">
-    <input type="text" name="losenord">
+    anamn<input type="text" name="anamn">
+    <br>
+    losenord<input type="text" name="losenord">
+    <br>
+    fnamn<input type="text" name="fnamn">
+    <br>
+    enamn<input type="text" name="enamn">
+    <br>
+    email<input type="text" name="email">
     <br>
     <select name="UID">
     <?php
@@ -18,18 +25,21 @@
                 echo "<option value='". $row["id"] ."'>id: ". $row['id']."</option>";
             }
             echo "</table>";
-            } else { echo "0 results"; }
+        } else { echo "0 results"; }
+        
         
     ?>
     </select>
     <br>
+    
+
     <input type="submit" value="Redigera Konto">
 </form>
 <br>
 <br>
 <!-- Skapa konto -->
 
-<h4>Skapa konton för din tjänst</h4>
+<h4>Skapa konton för din tjänst (Sadmin)</h4>
 
 
 <form action="funktioner/skapa.php" method="post">
@@ -53,6 +63,48 @@
     </select>
     <br>
     <input type="submit" value="Skapa Konto">
+</form>
+<br>
+<br>
+<!-- Redigera konto (user) -->
+
+<h4>Redigera kontoredigerare (Sadmin)</h4>
+
+
+<form action="funktioner/redigera.php" method="POST">
+<input type='hidden' name='funktion' value='redigeraRoll'/>
+
+    <br>
+    <select name="UID">
+    <?php
+        include('funktioner/dbh.inc.php');
+        $sql = "SELECT * from anvandare where aktiv = true";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<option value='". $row["id"] ."'>id: ". $row['id']."</option>";
+            }
+            echo "</table>";
+        } else { echo "0 results"; }
+        
+        
+    ?>
+    </select>
+    <br>
+    <select name="rollid">
+    <?php
+    $sql2 = "SELECT * from roll where id != 1";
+        $result2 = $conn->query($sql2);
+        if ($result2->num_rows > 0) {
+            while($row = $result2->fetch_assoc()) {
+                echo "<option value='". $row["id"] ."'>Roll: ". $row['rollNamn']."</option>";
+            }
+            echo "</table>";
+        } else { echo "0 results"; }
+    ?>
+    </select>
+    <br>
+    <input type="submit" value="Redigera roll">
 </form>
 <br>
 <br>
