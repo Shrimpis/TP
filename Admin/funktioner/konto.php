895133c81f6c24ab3f12471df0386b0088217815
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-15 13:20:22 +0200

    Lagt till deaktiveraKonto.

--- Admin/funktioner/konto.php
+++ Admin/funktioner/konto.php
@@ -8,6 +8,9 @@ include("dbh.inc.php");
             case 'aktiveraKonto':
                 aktiveraKonto();
                 break;
+            case 'deaktiveraKonto':
+                deaktiveraKonto();
+                break;
             default:
                 echo "ERROR: Något fel med URL-parametrarna för din begäran. Kontrollera dokumentationen.";
         }
@@ -20,17 +23,33 @@ function avslutaKonto(){
 
     $id = mysqli_real_escape_string($conn, $_POST['id']); //Kund-ID
 
-    $avsluta = "";
+    $avsluta= "";
 
-    if(mysqli_query($conn, $aktivera)){
+    if(mysqli_query($conn, $avsluta)){
         header('location: ../index.php?funktion=avslutaKonto?m=success');
     } else {
         echo "ERROR: Could not able to execute $avsluta. " . mysqli_error($conn);
     }
-
+    $conn->close();
 }
 
 function aktiveraKonto(){
+    include("dbh.inc.php");
+
+    $id = mysqli_real_escape_string($conn, $_POST['id']); //Kund-ID
+    $aktiv = '1';
+
+    $aktivera= "UPDATE `kund` SET `aktiv` = '$aktiv' WHERE `kund`.`id` = $id";
+
+    if(mysqli_query($conn, $aktivera)){
+        header('location: ../index.php?funktion=aktiveraKonto?m=success');
+    } else {
+        echo "ERROR: Could not able to execute $aktivera. " . mysqli_error($conn);
+    }
+    $conn->close();
+}
+
+function deaktiveraKonto(){
     include("dbh.inc.php");
 
     $id = mysqli_real_escape_string($conn, $_POST['id']); //Kund-ID
@@ -38,11 +57,12 @@ function aktiveraKonto(){
 
     $deaktivera= "UPDATE `kund` SET `aktiv` = '$aktiv' WHERE `kund`.`id` = $id";
 
-    if(mysqli_query($conn, $aktivera)){
+    if(mysqli_query($conn, $deaktivera)){
         header('location: ../index.php?funktion=deaktiveraKonto?m=success');
     } else {
         echo "ERROR: Could not able to execute $deaktivera. " . mysqli_error($conn);
     }
+    $conn->close();
 }
 
 ?>
