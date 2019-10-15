f212827c359309788b1e884671f48906154b6710
adolf pizzamannen <joel.magnusso@elev.ga.ntig.se>
2019-10-15 12:17:06 +0200

    glömde använda branch

--- Admin/funktioner/skapa.php
+++ Admin/funktioner/skapa.php
@@ -7,12 +7,17 @@ include("dbh.inc.php");
 
 
     
-        switch ($_POST['funktion']) {
+        switch ($_GET['funktion']) {
             case 'skapaKonto':
                 skapaKonto();
                 break;
+            case 'skapaAKonto':
+                skapaAKonto();
+                break;
 
+            default:    
                 echo "ERROR: Något fel med URL-parametrarna för din begäran. Kontrollera dokumentationen.";
+                break;
         }
     
  
@@ -21,8 +26,8 @@ $conn->close();
 function skapaKonto(){
     include("dbh.inc.php");
     $uname_tagen=false;
-    if(isset($_POST['uname'])){
-        $username= $_POST['uname'];
+    if(isset($_POST['anamn'])){
+        $username= $_POST['anamn'];
     }
     $password = slumplosen(10);
     
@@ -54,7 +59,34 @@ function slumplosen($len) {
     }
     return $slumpstr;
 }
-
+function skapaAKonto(){
+    include("dbh.inc.php");
+    $uname_tagen=false;
+    if(isset($_GET['anamn'])&&isset($_GET['rollid'])){
+        $username = $_GET['anamn'];
+        $rollid = $_GET['rollid'];
+    }
+    $password = slumplosen(10);
+    
+    $sqlcheck = ($conn->query("SELECT anamn from anvandare"));
+    while($row=$sqlcheck->fetch_assoc()){
+        if($username==$row['anamn']){
+            $uname_tagen=true;
+        }
+    }
+    if($uname_tagen==false){
+        $sql= "INSERT INTO anvandare(anamn, losenord) VALUES ('$username','$password')";
+        var_dump($sql);
+        $conn->query($sql);
+        header('Refresh: 5; URL = ../kontoformsadmin.php');
+    }
+    else{
+        echo "användarnamnet är taget faktiskt, ifall du inte visste det (men nu vet du)!";
+        header('Refresh: 5; URL = ../kontoformsadmin.php');
+    }
+    $conn->close();
+    
+}
 
 
 
--- Admin/kontoformsadmin.php
+++ Admin/kontoformsadmin.php
@@ -27,3 +27,32 @@
 </form>
 <br>
 <br>
+<!-- Skapa konto -->
+
+<h4>Skapa konton för din tjänst</h4>
+
+
+<form action="funktioner/skapa.php" method="get">
+<input type='hidden' name='funktion' value='skapaAKonto'/>
+    <input type="text" name="anamn">
+    
+    <br>
+    <select name="rollid">
+    <?php
+        include('funktioner/dbh.inc.php');
+        $sql = "SELECT * from roll where id != 1";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<option value='". $row["id"] ."'>Roll: ". $row['rollNamn']."</option>";
+            }
+            echo "</table>";
+            } else { echo "0 results"; }
+        
+    ?>
+    </select>
+    <br>
+    <input type="submit" value="Skapa Konto">
+</form>
+<br>
+<br>
