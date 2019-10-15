7e2a83dab2cec251a0460005fdd5991745116adf
Alvar Ljungfjär <ljungfjaralvar@gmail.com>
2019-10-15 11:04:33 +0200

    Fixade ett merge problem

--- Blogg/js/visaBlogg.js
+++ Blogg/js/visaBlogg.js
@@ -1,5 +1,7 @@
+
 let jsonData;
 let kommentarArray = new Array();
+let index = 0;
 
 
 function createBlogg() {
@@ -7,7 +9,8 @@ function createBlogg() {
     createTitel();
     createInlagg();
     createSkribent();
-    createKommentar();
+    
+    console.log(jsonData);
 }
 
 
@@ -77,8 +80,8 @@ function createInlagg(id) {
                 element.className = "ruta bildRuta";
                 element.id = "ruta" + j;
                 let element2 = document.createElement("img");
-                element2.src = jsonData.bloggInlagg[i].rutor[j].bild;
-                element2.alt = "bild";
+                element2.src = "images/kys.jpg";
+                element2.alt = "KYS";
                 element.appendChild(element2);
             }
             
@@ -93,100 +96,50 @@ function createInlagg(id) {
 
 
 //Inte dynamisk
-function createKommentar() {
+function createKommentar(kom) {
     let body = document.getElementById("kommentarContainer");
     let kommentar = document.createElement("div");
 
-    for(let i = 0; i < jsonData.bloggInlagg.length; i++)
-        for (let j = 0; j < jsonData.bloggInlagg[0].kommentarer.length; j++) {
+   
 
             let element = document.createElement("div");
-            element.id = "kommentar" + j;
+            element.id = "kommentar";
             let element2;
-            
-            if (jsonData.bloggInlagg[0].kommentarer[j].hierarchyID == 0) {
-                element2 = document.createElement("p");
-                element2.innerHTML = jsonData.bloggInlagg[i].kommentarer[j].namn + ": " + jsonData.bloggInlagg[0].kommentarer[j].text;
-                element.appendChild(element2);
-            } else {
-                element2 = document.createElement("div");
-                let element3 = document.createElement("p");
-                element3.innerHTML = jsonData.bloggInlagg[i].kommentarer[j].namn + ": " + jsonData.bloggInlagg[0].kommentarer[j].text;
 
-                element2.appendChild(element3);
-                element.appendChild(element2);
-            }
-            
+            element2 = document.createElement("p");
+            element2.innerHTML = kom.namn + ": " + kom.text + " : " + kom.hierarchyID;
+            element.appendChild(element2);
+
             kommentar.appendChild(element);
   
-    }
-    
+
     body.appendChild(kommentar);
 }
 
 
-
-<<<<<<< HEAD:visaBlogg.js
-=======
-//Work in progress fully dynamisk kommentarsfält
-/*function createKommentar() {
-    let body = document.getElementById("kommentarContainer");
-    let kommentar = document.createElement("div");
-    
-    for(let i = 0; i < jsonData.bloggInlagg.length; i++)
-    {
-        for (let j = 0; j < jsonData.bloggInlagg[i].kommentarer.length; j++) {
-            //console.log(jsonData.bloggInlagg[i].kommentarer[j].text);
-            if(jsonData.bloggInlagg[i].kommentarer[j].hierarchyID == 0) {
-                let element = rekurs(jsonData.bloggInlagg[i].kommentarer[j].KID); 
+function recurs(kom){
+    if (kom instanceof Object) {
+        for (let k in kom){
+            if (kom.hasOwnProperty("kommentarer")){
+                if(k == "kommentarer"){
+                    if(kom[k][index].kommentarer.length > 0){
+                        for(let i = 0; i < kom[k].length; i++){
+                            createKommentar(kom[k][i]);
+                        }
+                        recurs(kom[k][index]);
+                    }else{
+                        createKommentar(kom[k][index]);
+                        index++;
+                        recurs(kom[k]);
+                    }
                 
-                kommentar.appendChild(element); 
-            }
-            
-        } 
+                }
+            }                
+        }
     }
-    
-    //console.log(kommentar);
-    body.appendChild(kommentar);
-    
 }
-function rekurs(id) {
-    let element, element2;
-    kommentarArray.push(id);
-    for(let i = 0; i < jsonData.bloggInlagg.length; i++) {
-        
-        for (let j = 0; j < jsonData.bloggInlagg[i].kommentarer.length; j++) {
-            element = document.createElement("div");
-            element.id = "kommentar" + j;
-            
-            //console.log(jsonData.bloggInlagg[i].kommentarer[j].hierarchyID + " " + id);
-            if(jsonData.bloggInlagg[i].kommentarer[j].hierarchyID == id) {
-                kommentarArray.push(jsonData.bloggInlagg[i].kommentarer[j].hierarchyID);
-            
-            }
-            
-        }
-    }
-    for(let i = 0; i < jsonData.bloggInlagg.length; i++){
-        for(let j = 0; j < jsonData.bloggInlagg[i].kommentarer.length; j++) {
-            console.log(kommentarArray.length);
-            while(kommentarArray.length > 0) {
-                element2 = document.createElement("p");
-                element2.innerHTML = jsonData.bloggInlagg[i].kommentarer[j].text;
-                element.appendChild(element2);
-                console.log(jsonData.bloggInlagg[i].kommentarer[j].text);
-                kommentarArray.splice(i);
-            }
-        }
-    }
-    console.log(element);
-    return element;
-    
-}*/
 
 
-
->>>>>>> 4714569e9bdba91bc48dbfc0e5c0e7e6a96ac0a3:js/visaBlogg.js
 function init() {
 
     let xhttp = new XMLHttpRequest();
@@ -196,9 +149,10 @@ function init() {
         if (this.readyState === 4 && this.status === 200) {
             
             jsonData = JSON.parse(this.responseText);
-            console.log(jsonData);
+            //console.log(jsonData);
             //console.log(jsonData.titel);
             createBlogg();
+            recurs(jsonData.bloggInlagg[0]);
         }
     };
 
