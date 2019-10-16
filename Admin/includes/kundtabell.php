032c0757466245f515dcbb32c85213eb98bb0e1f
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-16 09:35:10 +0200

    Tog bort password eftersom att den inte behövs.

--- Admin/includes/kundtabell.php
+++ Admin/includes/kundtabell.php
@@ -79,15 +79,6 @@
                             </div>
                             <input type='text' class='form-control' placeholder='Användarnamn' aria-label='Användarnamn' name='anamn' aria-describedby='basic-addon1' required autofocus>
                             </div>
-                            <div class='input-group mb-3'>
-                            <div class='input-group-prepend'>
-                                <span class='input-group-text' id='basic-addon1'><i class='fas fa-key'></i></span>
-                            </div>
-                            <input id='losenord' type='text' class='form-control' placeholder='Lösenord' aria-label='Lösenord' name='losenord' aria-describedby='basic-addon1' required autofocus>
-                            <div class='input-group-prepend'>
-                                <input type='button' value='Generate' class='input-group-text' id='basic-addon1' onClick='generate();' tabindex='2'></input>
-                            </div>
-                            </div>
                         <button type='submit' class='btn btn-primary btn-sm' style='margin-top:4px;margin-bottom:10px;'>Skapa konto</button>
                         </form>
                         ";
