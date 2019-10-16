b0edbed792959cf0c134689f2b78656826883334
Brandon Mogren <brandon.mogren@learnet.se>
2019-10-16 09:44:09 +0200

    mergar masterl med masterr

--- Blogg/js/visaBlogg.js
+++ Blogg/js/visaBlogg.js
@@ -3,16 +3,14 @@ let jsonData;
 let kommentarArray = new Array();
 let index = 0;
 
-
 function createBlogg() {
 
     createTitel();
     createInlagg();
     createSkribent();
     
-    console.log(jsonData);
-}
 
+}
 
 
 //Dynamisk
@@ -28,7 +26,6 @@ function createSkribent() {
 }
 
 
-
 //Dynamisk
 function createTitel() {
     let body = document.getElementById("headerContainer");
@@ -42,53 +39,14 @@ function createTitel() {
 }
 
 
-
 function createInlagg(id) {
     
     let body = document.getElementById("bloggInlaggContainer");
     let inlagg = document.createElement("div");
-    //console.log(jsonData.bloggInlagg[0].titel);
+    console.log(jsonData);
     inlagg.id = "inlagg" + id;
-    for (let i = 0; i < jsonData.bloggInlagg.length; i++) {
-        
-        let titleContainer = document.createElement("div");
-        let title = document.createElement("h2");
-        title.innerHTML = jsonData.bloggInlagg[i].titel;
-        titleContainer.appendChild(title);
-        inlagg.appendChild(titleContainer);
-        for (let j = 0; j < jsonData.bloggInlagg[i].rutor.length; j++) {
 
-            let element = document.createElement("div");
-            
-            let element2;
-            
-            if (jsonData.bloggInlagg[i].rutor[j].type === "textRuta") {
 
-                element.className = "ruta textRuta";
-                element.id = "inlagg" + id + "ruta" + j;
-                if (jsonData.bloggInlagg[i].rutor[j].rubrik !== null) {
-
-                    element2 = document.createElement("h3");
-                    element2.innerHTML = jsonData.bloggInlagg[i].rutor[j].rubrik;
-                    element.appendChild(element2);
-                }
-
-                element2 = document.createElement("p");
-                element2.innerHTML = jsonData.bloggInlagg[i].rutor[j].text;
-                element.appendChild(element2);
-            } else {
-                element.className = "ruta bildRuta";
-                element.id = "ruta" + j;
-                let element2 = document.createElement("img");
-                element2.src = "images/kys.jpg";
-                element2.alt = "KYS";
-                element.appendChild(element2);
-            }
-            
-            inlagg.appendChild(element);
-        }
-            inlagg.appendChild(document.createTextNode(jsonData.bloggInlagg[i].datum));
-    }
     
     body.appendChild(inlagg);
 }
@@ -149,16 +107,16 @@ function init() {
         if (this.readyState === 4 && this.status === 200) {
             
             jsonData = JSON.parse(this.responseText);
-            //console.log(jsonData);
+            //console.log(this.responseText);
             //console.log(jsonData.titel);
             createBlogg();
             recurs(jsonData.bloggInlagg[0]);
         }
     };
 
-    xhttp.open("GET", "json/bloggjson.php?visa=anvandare&anvandare=1&blogg=1", true);
+    xhttp.open("GET", "json/bloggjson.php?anvandare=2&blogg=2&inlagg=2", true);
     xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
     xhttp.send();
     }
 
-document.body.onload = function() {init()};
+//document.body.onload = function() {init();};
