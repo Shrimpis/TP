d7e15fc98898209aad95b3160573b7746f49371d
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-18 13:34:12 +0200

    Ändra till de nya variablerna

--- Admin/funktioner/aktivera.php
+++ Admin/funktioner/aktivera.php
@@ -16,11 +16,9 @@ function aktiveraTjanst(){
     include("dbh.inc.php");
 
     $id = mysqli_real_escape_string($conn, $_POST['id']); //Kund-ID
-    $bloggCheck = (isset($_POST['bloggCheck'])) ? 1 : 0;
-    $wikiCheck = (isset($_POST['wikiCheck'])) ? 1 : 0;
-    $kalenderCheck = (isset($_POST['kalenderCheck'])) ? 1 : 0;
+    $tjanst = (isset($_POST['CheckTjanst'])) ? 1 : 0;
 
-    $aktivera = "UPDATE `kund` SET `blogg` = '$bloggCheck', `wiki` = '$wikiCheck', `kalender` = '$kalenderCheck' WHERE `kund`.`id` = $id";
+    $aktivera = "UPDATE `kundrattigheter` SET `tjanst` = '$tjanst' WHERE `kundrattigheter`.`id` = $id";
 
     if(mysqli_query($conn, $aktivera)){
         header('location: ../index.php?funktion=aktiveraTjanst?m=success');
