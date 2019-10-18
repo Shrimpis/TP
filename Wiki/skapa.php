510b43b0187b4c84fbac25cc10426ff53d3cb0b6
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2019-10-18 14:18:02 +0200

    skapade skapWikiUppdateringsfunktionen i skapa.php i wiki

--- Wiki/skapa.php
+++ Wiki/skapa.php
@@ -19,9 +19,24 @@ switch($_POST['funktion']){
 
 }
 
+$conn->close();
+
 function skapaWikiUppdatering(){
 
+    include("dbh.inc.php");
+    if(isset($_POST['sidId']) && isset($_POST['bidragsgivare']) && isset($_POST['titel']) && isset($_POST['innehall'])){
 
+        $sidID = $_POST['sidId'];
+        $bidragsgivare = $_POST['bidragsgivare'];
+        $titel = $_POST['titel'];
+        $innehall = $_POST['innehall'];
+
+    }
+
+    $datum = date("Y-m-d H:i");
+    $skapaUppdatering = "INSERT INTO wikiuppdatering(sidId, bidragsgivare, titel, innehall, datum) VALUES('$sidID', '$bidragsgivare', '{$titel}', '{$innehall}', '$datum')";
+    mysqli_query($conn, $skapaUppdatering);
+    $conn->close();
 
 }
 
