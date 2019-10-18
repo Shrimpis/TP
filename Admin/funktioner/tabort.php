169bf81c47d44b8d3e2f973c0ef14d4981f7bebc
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-18 10:56:32 +0200

    Lagt till mysqli_query i harddelkonto.

--- Admin/funktioner/tabort.php
+++ Admin/funktioner/tabort.php
@@ -43,8 +43,8 @@ function harddelkonto(){
     $delgil = "DELETE FROM gillningar WHERE anvandarId ='{$id}'";
     
     $aktiv = '0';
-    $deaktiverakonto= "UPDATE `anvandare` SET `aktiv` = '$aktiv' WHERE `anvandare`.`id` = $id";
-    $deaktiveratjanst = "UPDATE `kund` SET `blogg` = '$aktiv', `wiki` = '$aktiv', `kalender` = '$aktiv', `aktiv` = '$aktiv' WHERE `kund`.`id` = $id";
+    $deaktiverakonto= mysqli_query($conn,"UPDATE `anvandare` SET `aktiv` = '$aktiv' WHERE `anvandare`.`id` = $id");
+    $deaktiveratjanst = mysqli_query($conn,"UPDATE `kund` SET `blogg` = '$aktiv', `wiki` = '$aktiv', `kalender` = '$aktiv', `aktiv` = '$aktiv' WHERE `kund`.`id` = $id");
     
     $result = $conn->query("SELECT id from tjanst where anvandarId = '{$id}'");
     if(mysqli_num_rows($result) > 0){
