09d5b534b731368de15bd5b4938b99261505c8c9
adolf pizzamannen <joel.magnusso@elev.ga.ntig.se>
2019-10-22 10:49:58 +0200

    fixade så att nekauppdatering tar bort från uppdateringar

---
+++ Wiki/funktioner/nekawiki.php
@@ -0,0 +1,24 @@
+<?php
+
+nekaUppdatering();
+function nekaUppdatering(){
+
+include("dbh.inc.php");
+if(isset($_POST['sidId'])&&isset($_POST['denier'])){
+    $sidID = $_POST['sidId'];
+    $denying_me_the_rank_of_master = $_POST['denier'];
+    $its_reason_then = $_POST['reason'];
+}
+$get_data=$conn->query("SELECT * FROM wikiuppdatering WHERE id='{$sidID}'");
+$row = $get_data->fetch_assoc();
+$datum = date("Y-m-d H:i");
+$badcontributor = $row['bidragsgivare'];
+$titel = $row['titel'];
+$innehall = $row['innehall'];
+$nekaUppdatering = "INSERT INTO nekadwikiuppdatering(sidId, bidragsgivare, nekadAv, titel, innehall, anledning, datum) VALUES('$sidID', '$badcontributor', '$denying_me_the_rank_of_master', '{$titel}', '{$innehall}', '{$its_reason_then}', '$datum')";
+$tabortuppdatering = "DELETE FROM wikiuppdatering WHERE id='{$sidID}'";
+mysqli_query($conn, $nekaUppdatering);
+mysqli_query($conn, $tabortuppdatering);
+$conn->close();
+
+}
