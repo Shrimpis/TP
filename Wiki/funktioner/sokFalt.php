4a59002e1e451434eaa7038e1cbfafe77266e928
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2019-10-23 10:40:29 +0200

    Lade in databaskoppling i sokFalt samt lade in allt från funktionen in i isset

--- Wiki/funktioner/sokFalt.php
+++ Wiki/funktioner/sokFalt.php
@@ -11,22 +11,25 @@ mysqli_set_charset($conn, "utf8mb4");
 //här söker man för bloggInlägg i database.
 function sokFalt(){
 
+    include('dbh.inc.php');
+
     if(isset($_POST['sok'])){
           
         $sok= $_POST['sok'];
     
-
+        $query = mysqli_query($conn,"SELECT * FROM wikisidor WHERE titel LIKE '%$sok%'") or die ("Could not search");
+        if($count = mysqli_num_rows($query)){
+    
+    
+    
+        }else if($count == 0){
+            $output = "There was no search results!";
+        }
 
     } 
 
-    $query = mysqli_query($conn,"SELECT * FROM wikisidor WHERE titel LIKE '%$sok%'") or die ("Could not search");
-    if($count = mysqli_num_rows($query)){
+    $conn->close();
 
-
-
-    }else if($count == 0){
-        $output = "There was no search results!";
-    }
 }
 
 
