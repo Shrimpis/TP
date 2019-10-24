280352c80d07b09f6c24a136c9efa6d194da3250
Joelpizza <joelpizza1945@gmail.com>
2019-10-24 13:39:41 +0200

    kalendereventskapa funkar

--- Kalender/index.php
+++ Kalender/index.php
@@ -89,6 +89,60 @@
         <br>
         <br>
 
+        <!-- skapa event -->
+
+        <h4>Skapa kalenderevent</h4>
+
+        <form action="funktioner/skapa.php" method="POST">
+        <input type='hidden' name='funktion' value='skapaKalenderevent'/>
+        välj en användare:
+            <select name="anvandarId">
+            <?php
+                include("funktioner/dbh.inc.php");
+                $sql = "SELECT * from anvandare";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo "<option value='" . $row['id'] . "'>" . $row['id'] . "</option>";
+                    }
+                } else { 
+                    echo "0 results"; 
+                }
+                $conn->close();
+                
+            ?>
+            </select>
+            <br>
+            kalender: 
+            <select name="kalenderId">
+            <?php
+                include("funktioner/dbh.inc.php");
+                $sql = "SELECT * from kalender";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo "<option value='" . $row['tjanstId'] . "'>" . $row['tjanstId'] . "</option>";
+                    }
+                } else { 
+                    echo "0 results"; 
+                }
+                $conn->close();
+                
+            ?>
+            </select>
+            <br>
+            Titel: <input type="text" name="titel">
+            <br>
+            Innehåll: <input type="text" name="innehall">
+            <br>
+            start: <input type="text" name="startTid">
+            <br>
+            slut: <input type="text" name="slutTid">
+            <br>
+            <input type="submit" value="skapa kalenderevent">
+        </form>
+        <br>
+        <br>
 
     </body>
 </html>
