096c754e16453d00ce747506188eca5b27c23e24
Hossam Alalian <hossam.alalian@learnet.se>
2019-10-25 14:03:59 +0200

    har gjort att man kan aktiver en event

--- Kalender/funktioner/redigera.php
+++ Kalender/funktioner/redigera.php
@@ -4,13 +4,47 @@
 
 session_start();
 
-include('../../Databas/dbh.inc.php');
+include('./Databas/dbh.inc.php');
         switch ($_POST['funktion']) {
 
-            case '':
-                ();
+            case 'aktiveraEvent':
+                aktiveraEvent($conn);
                 break;
 
             default:
                 echo "ERROR: Något fel med URL-parametrarna för din begäran. Kontrollera dokumentationen.";
+        }
+
+
+        function aktiveraEvent($conn){
+            //-include("../../Databas/dbh.inc.php");
+            if(isset($_POST['id']) ){
+                $id = $_POST['id'];
+            }
+            $event = $conn->query('select * from event where id ='.$id);
+        
+                while($row = $event->fetch_assoc()){
+                    $aktiv=$row["aktiv"];
+                    if($aktiv==0){
+                        $sql= "UPDATE event SET aktiv = '1' WHERE id = $id ";
+                        $conn->query($sql);
+                        break;
+                    }
+                    else if($aktiv==1){
+                        $sql= "UPDATE event SET aktiv = '0' WHERE id = $id ";
+                        $conn->query($sql);
+                        echo "blah";
+                       break;
+                    }
+
+                    else{
+                        echo "hej";
+                    }
+                }
+                
+                
+                   
+                    
+                
+                    
         }
