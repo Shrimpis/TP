541c8da0d5a8ae148d6da5df70c0f6ccd151ec2a
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2019-10-28 15:24:34 +0100

    Ändrade tabortBloggfunktione i att det är bara en SELECT och DELETE istället för 9 sql statement

--- Blogg/funktioner/tabort.php
+++ Blogg/funktioner/tabort.php
@@ -29,91 +29,16 @@ function tabortBlogg($conn){
     if(isset($_POST['bloggId'])){
     $bloggId = $_POST['bloggId'];
 
-    $taBortTjanst = "DELETE FROM tjanst WHERE id='{$bloggId}'";
-    $taBortFlaggningBlogg = "DELETE FROM flaggadblogg WHERE bloggId = '{$bloggId}'";
-    $taBortBlogg = "DELETE FROM blogg WHERE tjanstId='{$bloggId}'";
-    $taBortInlagg = "DELETE FROM blogginlagg WHERE bloggId='{$bloggId}'";
-    $inlaggsId = ($conn->query("SELECT id FROM blogginlagg WHERE bloggId = $bloggId"));
-    $kommentarId = ($conn->query("SELECT id FROM kommentar WHERE inlaggId = $inlaggsId"));
-    $taBortFlaggningKommentar = "DELETE FROM flaggadkommentar WHERE kommentarId = '{$kommentarId}'";
-    $taBortKommentar = "DELETE FROM kommentar WHERE inlaggId = $inlaggsId";
-    $taBortLike = "DELETE FROM gillningar WHERE inlaggId = $inlaggsId";
+    $tjanstId = ($conn->query("SELECT tjanstId FROM blogg WHERE id = $bloggId"));
+    $taBortBlogg = "DELETE FROM tjanst WHERE id='{$tjanstId}'";
 
-    
-
-    if(mysqli_query($conn, $taBortFlaggningBlogg)){
-
-        hantering('200','Flaggning på bloggen har tagits bort',);
-
-    }else{
-
-        hantering('400','Flaggning på bloggen kunde inte tas bort',);
-
-    }
-    while($row = $kommentarId->fetch_assoc()){
-        $kommentarId = $row['id'];
-
-        if(mysqli_query($conn, $taBortFlaggningKommentar)){
-
-            hantering('200','Flaggningen på kommentaren har tagits bort',);
-
-        }else{
-
-            hantering('400','Flaggningen på kommentaren kunde inte tas bort',);
-
-        }
-
-    }
-    while($row = $inlaggsId->fetch_assoc()){
-        $inlaggsId= $row['id'];
-    
-        if(mysqli_query($conn, $taBortKommentar)){
-
-            hantering('200','Kommentaren togs bort',);
-
-        }else{
-
-            hantering('400','Kommentaren kunde inte tas bort',);
-            return;
-
-        }
-        if(mysqli_query($conn, $taBortLike)){
-
-            hantering('200','Inlägget gillas inte längre',);
-
-        }else{
-
-            hantering('400','Kunde inte ändra gilla statusen på inlägget',);
-            return;
-
-        }
-        
-    }
-    if(mysqli_query($conn, $taBortInlagg)){
-
-        hantering('200','Blogginlägget har tagits bort',);
-
-    }else{
-
-        hantering('400','Blogginlägget kunde inte tas bort',);
-        
-    }
     if(mysqli_query($conn, $taBortBlogg)){
 
         hantering('200','Bloggen har tagits bort',);
 
-    } else {
-
-        hantering('400','Bloggen kunde inte tas bort',);
-
-    }
-    if(mysqli_query($conn, $taBortTjanst)){
-
-        hantering('200','Tjänsten har tagits bort',);
-
     }else{
 
-        hantering('400','Tjänsten kunde inte tas bort',);
+        hantering('400','Bloggen kunde inte tas bort',);
 
     }
 
