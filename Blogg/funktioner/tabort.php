cbe2aba09eff6290ed710bf9de3c9f8e45e80a8e
Viktor Karlsson Ferm <viktor.karlssonferm@elev.ga.ntig.se>
2019-10-28 09:54:50 +0100

    Lade till felhantering i switch statement i tabort.php i blogg

--- Blogg/funktioner/tabort.php
+++ Blogg/funktioner/tabort.php
@@ -20,7 +20,7 @@ include("../../Databas/dbh.inc.php");
                 tabortTextruta($conn);
                 break;
             default:
-                echo "ERROR: Något fel med URL-parametrarna för din begäran. Kontrollera dokumentationen.";
+                hantering('400','Din förfrågan är utanför våra parametrar, kolla dokumentationen',);
         } 
 
 
