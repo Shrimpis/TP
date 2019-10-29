18905d7cb50ce4897f39ddfb789a893f3b1b6d0b
Shrimpis <ljungfjaralvar@gmail.com>
2019-10-29 13:44:07 +0100

    fixa lite på visa kalender

---
+++ Kalender/visaKalender.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div id="kalenderContainer" class="kalenderContainer">
+        <div id="titelContainer" class="titelContainer"></div>
+        <div id="sidContainer" class="sidContainer"></div>
+        <br><br>
+        <div id="eventContainer" class="eventContainer"></div>
+    </div>    
+    
+</body>
+
+<script>
+
+    let anvandarId = <?php echo $_GET['anvandarId'] ?>;
+    let kalenderId = <?php echo $_GET['kalenderId'] ?>;
+    let sidId = <?php echo $_GET['sidId'] ?>;
+
+</script>
+
+
+<script src="js/visaKalender.js"></script>
+</html>
