369f1879e5b4bbe4656e78a3105e9f272f0d4c79
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-30 09:05:43 +0100

    Lagt till redigerAKonto.

--- Admin/funktioner/redigera.php
+++ Admin/funktioner/redigera.php
@@ -12,10 +12,83 @@ include("../../Databas/dbh.inc.php");
             case 'redigeraRoll':
                 redigeraRoll($conn);
                 break;
+            case 'redigeraAKonto':
+                redigeraAKonto($conn);
+                break;
             default:
                 echo "ERROR: Något fel med URL-parametrarna för din begäran. Kontrollera dokumentationen.";
                 break;
         } 
+
+function redigeraAKonto($conn){
+    $uname_tagen=false;
+
+    $uname = $_POST['anamn'];
+    $losenord = $_POST['losenord'];
+    $id = $_POST['id'];
+
+    $password = slumplosen(10);
+    
+        if (defined("CRYPT_BLOWFISH") && CRYPT_BLOWFISH) {
+            echo "CRYPT_BLOWFISH is enabled!<br>";
+        } else {
+            echo "CRYPT_BLOWFISH is NOT enabled!";
+        }
+        
+        $Blowfish_Pre = '$2a$10$';
+        $Blowfish_End = '$';
+        
+        $Allowed_Chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        $Chars_Len = 63;
+        
+        $Salt_Length = 21;
+        $salt = "";
+        for ($i = 0; $i < $Salt_Length; $i++) {
+            $salt .= $Allowed_Chars[mt_rand(0, $Chars_Len)];
+        }
+        $bcrypt_salt = $Blowfish_Pre . $salt . $Blowfish_End;
+        $hashed_password = crypt($password, $bcrypt_salt);
+        
+    
+    $sqlcheck = ($conn->query("SELECT anamn from anvandare"));
+    while($row=$sqlcheck->fetch_assoc()){
+        if($username==$row['anamn']){
+            $uname_tagen=true;
+        }
+    }
+    if($uname_tagen==false){
+
+            $sql= "INSERT INTO anvandare(anamn, losenord, salt) VALUES ('$username','$hashed_password','$salt')";
+            $sql = "UPDATE anvandare SET anamn = '$uname', losenord = '$hashed_password', salt = '$salt' WHERE id = '$id'";
+
+            $conn->query($sql);
+
+            // Skapar mappar för bilder //
+
+            $owner = 'theprovider';
+
+            $blogg = '/var/www/html/TP/Bilder/Blogg/'.$username.'';
+            $wiki = '/var/www/html/TP/Bilder/Wiki/'.$username.'';
+
+            if(!mkdir($blogg, 0777, true)){
+                chown($blogg, $owner);
+                header('location: ../index.php?funktion=skapaKonto?status=failed?reason=blogg_folder+exists');
+            }
+
+            if(!mkdir($wiki, 0777, true)){
+                chown($wiki, $owner);
+                header('location: ../index.php?funktion=skapaKonto?status=failed?reason=wiki_folder+exists');
+            }
+
+            header('location: ../index.php?funktion=skapaKonto?status=success');
+        }
+        
+    }
+    else{
+        header('location: ../index.php?funktion=skapaKonto?status=error?reason=usernameTaken');
+    }
+    $conn->close();
+}
 
 function redigeraKonto($conn){
     //include("../../Databas/dbh.inc.php");
@@ -40,9 +113,7 @@ function redigeraKonto($conn){
     }
     if(isset($_POST['fnamn'])){
         $fnamn = $_POST['fnamn'];
-        $conn-query("UPDATE anvandare SET fnamn = '{$fnamn}'  WHERE id = $anvandarid ");
-        
-        
+        $conn-query("UPDATE anvandare SET fnamn = '{$fnamn}'  WHERE id = $anvandarid "); 
     }
     }
 
