1645904a8002fdd55d67a5dde6367cadb49d5eb2
Brandon Mogren <brandon.mogren@learnet.se>
2019-10-30 12:36:25 +0100

    Lade till Auth för att authenticata anvandare

---
+++ Auth/auth.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+
+if(isset($_POST["anvandarnamn"]) && isset($_POST["losenord"])){
+
+      include("../Databas/dbh.inc.php");
+
+      $anamn = $_POST["anvandarnamn"];
+      $password = $_POST["losenord"];
+
+      $sql = "SELECT id, salt, losenord,anamn from anvandare where anamn='{$anamn}'";
+      $result = $conn->query($sql);
+
+      $row = mysqli_fetch_assoc($result);
+
+      if (defined("CRYPT_BLOWFISH") && CRYPT_BLOWFISH) {
+          echo "CRYPT_BLOWFISH is enabled!<br>";
+      } else {
+          echo "CRYPT_BLOWFISH is NOT enabled!";
+      }
+
+      $Blowfish_Pre = '$2a$10$';
+      $Blowfish_End = '$';
+
+
+
+      $bcrypt_salt = $Blowfish_Pre . $row["salt"] . $Blowfish_End;
+      $hashed_password = crypt($password, $bcrypt_salt);
+
+
+      if($hashed_password == $row["losenord"]){
+        $_SESSION["UID"] = $row["id"];
+      }
+
+}
