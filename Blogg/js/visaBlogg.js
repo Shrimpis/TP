b24c10c025e2110d29395b0a6fb8cbe372e10e67
theProviderServer <theProviderServer@out.com>
2019-10-30 13:10:57 +0100

    server ändringar

--- Blogg/js/visaBlogg.js
+++ Blogg/js/visaBlogg.js
@@ -6,22 +6,8 @@ let flagga;
 let bloggTitel;
 
 
-<<<<<<< HEAD
-    createTitel();
-    createInlagg();
-    createSkribent();
-    createFlagga();
 
-    
-
-
-}
-
-
-//Dynamisk
-=======
 //Dynamisk. Skriver ut vem som äger bloggen
->>>>>>> 9981f68c589da848b0ba77fb419c2e17e606c11d
 function createSkribent() {
     let body = document.getElementById("skribentContainer");
     let element = document.createElement("p");
@@ -88,13 +74,6 @@ function createInlagg(id) {
     let divText = document.createElement("div");
     divText.innerHTML = jsonData.innehall;
     inlagg.appendChild(divText);
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> 9981f68c589da848b0ba77fb419c2e17e606c11d
     body.appendChild(inlagg);
 
 
@@ -114,19 +93,10 @@ function createInlagg(id) {
 function createKommentar(kom) {
     let body = document.getElementById("bloggInlaggContainer");
     let kommentar = document.createElement("div");
-<<<<<<< HEAD
 
-
-
-
-
-            let element = document.createElement("div");
-            //element.id = "kommentarKommentar";
-            let element2;
-=======
     let element = document.createElement("div");
     let element2;
->>>>>>> 9981f68c589da848b0ba77fb419c2e17e606c11d
+
 
     element2 = document.createElement("p");
     element2.innerHTML = kom.namn + ": " + kom.innehall + " : " + kom.hierarkiId;
@@ -199,21 +169,6 @@ function next(id) {
 
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-<<<<<<< HEAD
-            
-            jsonData = JSON.parse(this.responseText);
-	    createBlogg();
-	    recurs(jsonData);
-	}
-    xhttp.open("GET", "json/bloggjson.php?anvandare=1&blogg=2&inlagg=2", true);
-    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-    xhttp.send();
-    }
-    }
-   // document.body.onload = function() {init();};
-
-=======
-
             jsonData = JSON.parse(this.responseText);
             
             //console.log(jsonData);
@@ -228,5 +183,3 @@ function next(id) {
     xhttp.send();
 }
     
-    document.body.onload = function() {init();};
->>>>>>> 9981f68c589da848b0ba77fb419c2e17e606c11d
