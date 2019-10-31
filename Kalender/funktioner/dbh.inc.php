9b3fa4481d25e18d5fc44bd1dab5534b8d52bcda
adolf pizzamannen <joel.magnusso@elev.ga.ntig.se>
2019-10-31 13:33:54 +0100

    tog bort onödig databaskoppling

--- Kalender/funktioner/dbh.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbServername = 'localhost';
-$dbUsername = 'user';
-$dbPassword = '';
-$dbName = 'TheProvider';
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-mysqlI_set_charset($conn, "utf8mb4");
-?>
