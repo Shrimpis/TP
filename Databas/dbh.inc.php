26a22997cbb3e8e634c333e276390dcc2125901a
Hossam Alalian <hossam.alalian@learnet.se>
2019-11-01 22:43:51 +0100

    gjorde några funktioner säkrare på Wiki

--- Databas/dbh.inc.php
+++ Databas/dbh.inc.php
@@ -1,8 +1,8 @@
 <?php 
 $dbServername = 'localhost';
-$dbUsername = 'root';//TheProvider
-$dbPassword = '';//lösenord
-$dbName = 'the_provider';
+$dbUsername = 'TheProvider';//TheProvider
+$dbPassword = 'lösenord';//lösenord
+$dbName = 'TheProvider';
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 mysqlI_set_charset($conn, "utf8mb4");
