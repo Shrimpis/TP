dd5d28786053a8a002590523afa4d731775565f9
Nils Pettersson <nils.pettersson2@learnet.se>
2019-11-29 12:21:15 +0100

    asd

--- Blogg/funktioner/skapa.php
+++ Blogg/funktioner/skapa.php
@@ -11,11 +11,11 @@ include("../../json/felhantering.php");
     $apikey = mysqli_real_escape_string($conn,$_POST['nyckel']);
     $sql = "SELECT nyckel FROM api WHERE nyckel = '$apikey'";
     */
-    $result = mysqli_query($conn,$sql);
+    /*$result = mysqli_query($conn,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-    $count = mysqli_num_rows($result);
+    $count = mysqli_num_rows($result);*/
 
-    if($count == 1){
+    //if($count == 1){
         switch ($_POST['funktion']) {
             case 'skapaBlogg':
                 skapaBlogg($conn);
@@ -47,10 +47,10 @@ include("../../json/felhantering.php");
             default:
                 hantering('404','Din förfrågan är utanför våra parametrar, kolla dokumentationen',);
         }
-    }
+    /*}
     else{
         hantering('401','Behörighet saknas');
-    }
+    }*/
 /*}
 else{
     hantering('401','Behörighet saknas, tom api');
