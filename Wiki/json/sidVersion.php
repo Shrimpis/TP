42647f7a5050056c2aa3d4b660744d8852f1a773
Shrimpis <alvarljungfjar@gmail.com>
2019-12-04 09:31:54 +0100

    returnar nu anamnet på sidversioner

--- Wiki/json/sidVersion.php
+++ Wiki/json/sidVersion.php
@@ -22,7 +22,7 @@ function sidVersion($anvandarId, $sidId,$conn){
     $i=0;
 
     while($row = $sidversion->fetch_assoc()){
-        $sidVersioner[$i]=array('id'=>$row["id"],'godkantAv'=>$row['godkantAv'],'bidragsgivare'=>$row["bidragsgivare"],'titel'=>$row["titel"],'innehall'=>$row["innehall"], 'datum'=>$row["datum"]);
+        $sidVersioner[$i]=array('id'=>$row["id"],'sidId'=>$row['sidId'],'godkantAv'=>$row['godkantAv'],'bidragsgivare'=>$row["bidragsgivare"],'titel'=>$row["titel"],'innehall'=>$row["innehall"], 'datum'=>$row["datum"]);
         $i++;
     }
 
