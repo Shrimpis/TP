35f36ce93858baf424786575903e1f6d24b8212b
Brandon Mogren <brandon.mogren@learnet.se>
2019-12-05 15:28:08 +0100

    felsöker

--- Kalender/json/kalenderjson.php
+++ Kalender/json/kalenderjson.php
@@ -24,15 +24,15 @@
 
         $i=0;
         $kalenderArray;
-        while($row=$tjanst->fetch_assoc()){
-            //$kalender = $conn->query('select * from kalender where tjanstId='.$row['id']);
-            /*if($kalender->num_rows==1){
+        //while($row=$tjanst->fetch_assoc()){
+            $kalender = $conn->query('select * from kalender where tjanstId='.$row['id']);
+            if($kalender->num_rows==1){
                 $row1=$kalender->fetch_assoc();
 
                 $kalenderArray[$i]=array('id'=>$row1['id'],'titel'=>$row['titel'],'privat'=>$row['privat']);
                 $i++;
-            }*/
-        }
+            }
+        //}
 
         $json=json_encode($kalenderArray);
         echo $json;
