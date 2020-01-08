d07810c6d7965cc84864ebb46c85cca325e1c949
Shrimpis <alvarljungfjar@gmail.com>
2020-01-08 12:43:56 +0100

    lade till inloggnings data

--- Login/login.php
+++ Login/login.php
@@ -15,19 +15,19 @@
     $Blowfish_End = '$';
     
     $anvandarId = $row['id'];
-    
+    $mail = $row['email'];
 
     $bcrypt_salt = $Blowfish_Pre . $row["salt"] . $Blowfish_End;
     $hashed_password = crypt($password, $bcrypt_salt);
 
-    if($hashed_password == $row["losenord"]){
+    if($hashed_password == $row["losenord"] && $row['aktiv'] == 1){
         if($tjanst == 'blogg'){
             $blogg_sql = "SELECT * FROM blogg where anvandarId='{$anvandarId}'";
             $bloggRes = $conn->query($blogg_sql);
             $bloggRow = mysqli_fetch_assoc($bloggRes);
             $bloggId = $bloggRow['id'];//tjanstId
 
-            $verifyJson = array('success' => true, 'anamn' => $anamn, 'losenord' => $password,'bloggId' => "$bloggId", 'anvandarId' => $anvandarId);
+            $verifyJson = array('success' => true, 'anamn' => $anamn, 'losenord' => $password,'bloggId' => "$bloggId", 'anvandarId' => $anvandarId, 'mail' => $mail);
             echo json_encode($verifyJson); 
         }
         else if($tjanst == 'wiki' || $tjanst == 'kalender'){
@@ -41,9 +41,13 @@
             $rollRow = mysqli_fetch_assoc($rollRes);
             $roll = $rollRow['rollNamn'];
 
-            $verifyJson = array('success' => true, 'anamn' => $anamn, 'losenord' => $password,'roll' => "$roll", 'anvandarId' => $anvandarId);
+            $verifyJson = array('success' => true, 'anamn' => $anamn, 'losenord' => $password,'roll' => "$roll", 'anvandarId' => $anvandarId, 'mail' => $mail);
             echo json_encode($verifyJson);
         }
+    }
+    else if($row['aktiv'] == 0){
+        $verifyJson = array('success' => false, 'reason' => 'This account is no longer active');
+        echo json_encode($verifyJson);
     }else{
         $verifyJson = array('success' => false, 'reason' => 'Wrong credentials');
         echo json_encode($verifyJson);
